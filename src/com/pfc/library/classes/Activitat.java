2752a3189652cf56d08fe28be90f521914dd65e2
miguelmg <miguelmorgom@gmail.com>
2013-01-30 19:16:47 +0100

    Lector XML de prueba para arrancar actividad

---
+++ src/com/pfc/library/classes/Activitat.java
@@ -0,0 +1,21 @@
+package com.pfc.library.classes;
+
+public class Activitat {
+	
+	private String title;
+	private String recursos;
+	private int cantidad;
+	private String objectiu;
+	
+	public Activitat(){
+	
+	}
+	
+	public Activitat(String title, String recursos, int cantidad, String objectiu){
+		this.title = title;
+		this.recursos = recursos;
+		this.cantidad = cantidad;
+		this.objectiu = objectiu;
+	}
+	
+}
---
+++ src/com/pfc/library/classes/IOXML.java
@@ -0,0 +1,7 @@
+package com.pfc.library.classes;
+
+public class IOXML {
+	public Activitat readIOXML(String nombreArchivo){
+		return null;
+	}
+}
